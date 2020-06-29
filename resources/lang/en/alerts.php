4887c61a88c3e78f257a4a959bc5bb2a99ecf733
gautampatel5317 <dashrath7021@gmail.com>
2020-06-29 13:31:26 +0530

    flash alert message use from language file & also use language text use from language file, mysql table name use from config & minor changes

---
+++ resources/lang/en/alerts.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'password_success_message'  => 'Password changed successfully!',
+    'password_error_message'    => 'This is not your Current Password.',
+    'customer_update_message'    => 'The Customer has been updated successfully!',
+    'customer_add_message'  => 'The Customer has been created successfully!',
+    'state_add_message'  => 'The state has been created successfully!',
+    'state_update_message'  => 'The State has been updated successfully!',
+    'subscription_add_message'  => 'The subscription Type has been created successfully!',
+    'subscription_edit_message'  => 'The Subscription Type has been updated successfully!',
+
+    'subscription_name_add_message'  => 'The Subscription Name has been created successfully!',
+    'subscription_name_edit_message'  => 'The Subscription Name has been updated successfully!',
+    'subscription_name_delete_message'  => 'The Subscription Name has been deleted successfully!',
+    'role_add_message'  => 'The Role has been created successfully!',
+    'role_edit_message' => 'The Role has been updated successfully!',
+    'role_delete_message'  => 'The Role has been deleted successfully!',
+    'country_add_message'  => 'The country has been created successfully!',
+    'country_edit_message'  => 'The Country has been updated successfully!',
+    'cms_add_message'  => 'The Cms has been created successfully!',
+    'cms_edit_message'  => 'The cms has been updated successfully!',
+    'setting_edit_message'  => 'The Settings has been updated successfully!',
+    'email_template_edit_message'  => 'The email template has been updated successfully!',
+    'email_template_delete_message'  => 'The email template has been deleted successfully!',
+    'level_experience_add_message'  => 'The Level of Experience has been created successfully!',
+    'level_experience_edit_message'  => 'The Level of Experience has been updated successfully!',
+    'user_add_message'  => 'The User has been created successfully!',
+    'user_edit_message' => 'The User has been updated successfully!',
+    'user_delete_message'   => 'The User has been deleted successfully!',
+    'password_success_message'  => 'Password changed successfully!',
+    'password_error_message'  => 'This is not your Current Password.',
+    'permission_add_message'  => 'The permissions has been created successfully!',
+    'permission_edit_message'   => 'The permissions has been updated successfully!',
+    'permission_delete_message' => 'The permissions has been deleted successfully!',
+    'city_add_message'  => 'The city has been created successfully!',
+    'city_edit_message' => 'The City has been updated successfully!',
+    'ingredient_add_message'  => 'The Ingredient Warnings has been created successfully!',
+    'ingredient_edit_message'   => 'The Ingredient Warnings has been updated successfully!',
+    
+
+];
